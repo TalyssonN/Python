18f69e857f9aefa209acad14e71fbed1def9b1a4
Talysson Nogueira <110793575+TalyssonN@users.noreply.github.com>
2026-01-09 00:00:39 -0300

    CRUD Front-End - Back End em produção.

--- ESTUDOS/PROJETOS COM FLASK/routes.py
+++ ESTUDOS/PROJETOS COM FLASK/routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-from main import db, Usuario
+# from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template
+# from main import db, Usuario
 
 routes = Blueprint('routes', __name__)
 
@@ -7,22 +8,22 @@ routes = Blueprint('routes', __name__)
 def homepage():
     return render_template('site.html')
 
-@routes.route('/cadastrar', methods=['POST'])
-def cadastrar_usuario():
-    nome = request.form.get('nome')
-    email = request.form.get('email')
+# @routes.route('/cadastrar', methods=['POST'])
+# def cadastrar_usuario():
+#     nome = request.form.get('nome')
+#     email = request.form.get('email')
 
-    # Evita erro de email duplicado
-    usuario_existente = Usuario.query.filter_by(email=email).first()
-    if usuario_existente:
-        return "Email já cadastrado", 400
+#     # Evita erro de email duplicado
+#     usuario_existente = Usuario.query.filter_by(email=email).first()
+#     if usuario_existente:
+#         return "Email já cadastrado", 400
 
-    novo_usuario = Usuario(
-        nome=nome,
-        email=email
-    )
+#     novo_usuario = Usuario(
+#         nome=nome,
+#         email=email
+#     )
 
-    db.session.add(novo_usuario)
-    db.session.commit()
+#     db.session.add(novo_usuario)
+#     db.session.commit()
 
-    return redirect(url_for('routes.homepage'))
+#     return redirect(url_for('routes.homepage'))
